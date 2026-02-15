1cd57230fb9e7c6222eb8d14cdf236120c551d6b
Weslei Alves Dos Santos <wesleikhan@gmail.com>
2026-02-15 12:01:52 -0300

    feat: created connection with endpoints of Backend to write, read and evich Informations of products sale

--- src/services/sale/SaleService.js
+++ src/services/sale/SaleService.js
@@ -3,3 +3,15 @@ import {api} from "../core/api"
 export function MakeSale (saleDate) {
     return api.post("/sale/make", saleDate)
 }
+
+export function ToWriteInfosOfProductsSale (infosProductsSale) {
+    return api.post("/sale/info/products", infosProductsSale)
+}
+
+export function ReadInfosOfProductsSale () {
+    return api.get("/sale/info/products")
+}
+
+export function DeleteInfosOfProductsSale () {
+    return api.delete("/sale/info/products")
+}
