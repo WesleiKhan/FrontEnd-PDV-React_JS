53232d6a6b3d0fd29a4a05179930ce8948b485c6
Weslei Alves Dos Santos <wesleikhan@gmail.com>
2026-03-05 12:32:21 -0300

    feat: created styles moduleto ProductToEdit

--- src/pages/product/ProductsToEdit.js
+++ src/pages/product/ProductsToEdit.js
@@ -2,6 +2,8 @@ import ListProducts from "../../components/product/ListProducts";
 import {useGetProducts} from "../../components/hook/useGetProducts";
 import {useNavigate} from "react-router-dom";
 
+import styles from "./ProductToEdit.module.css"
+
 function ProductsToEdit() {
     const {productsGet, loading, error} = useGetProducts();
 
@@ -16,7 +18,10 @@ function ProductsToEdit() {
     if (error) return <p>Erro ao carregar operadores</p>;
 
     return (
-        <div>
+        <div className={styles.parent}>
+            <div>
+                <button className={styles.butto_register} onClick={() => navigate("/products/register")}>Registrar Produto</button>
+            </div>
             <ListProducts
                 objects={productsGet}
                 onSelectProduct={sendIdProduct}
