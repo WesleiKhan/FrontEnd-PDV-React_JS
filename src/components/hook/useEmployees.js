d927b8f192f059988f2c3c709dd6ecd1c63ae910
Weslei Alves Dos Santos <wesleikhan@gmail.com>
2026-03-22 14:42:26 -0300

    refactor: change variable name to real reflect objects kind

--- src/components/hook/useEmployees.js
+++ src/components/hook/useEmployees.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import { viewEmployees } from "../../services/employee/EmployeeService";
 
 export function useEmployees() {
-    const [users, setUsers] = useState([]);
+    const [employees, setEmployees] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         viewEmployees()
-            .then(res => setUsers(res.data))
+            .then(res => setEmployees(res.data))
             .catch(err => setError(err))
             .finally(() => setLoading(false));
     }, []);
 
-    return { users, loading, error };
+    return { employees, loading, error };
 }
